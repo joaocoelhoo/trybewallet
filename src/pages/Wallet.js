0ab565739b22938aa9a2fc1c6f19ba3de063acf0
Joao Coelho <joaopaulomelocoelho@gmail.com>
2022-04-02 14:16:08 -0300

    Header create for Wallet Page, requirement 3 ok

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { user } = this.props;
+    console.log(user);
+    return (
+      <div>
+        <div>TrybeWallet</div>
+        <div>
+          <header data-testid="email-field">{ user }</header>
+          <span data-testid="total-field">0</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  user: state.user.email,
+});
+Wallet.propTypes = {
+  user: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps, null)(Wallet);
